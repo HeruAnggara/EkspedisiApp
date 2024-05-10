4dcd464b1fe92129914bb9521cd962277bf5dcb7
Heru Anggara <heruanggara0909@gmail.com>
2024-05-10 22:30:09 +0700

    update new import module jenis pengeluaran

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersController } from './users/users.controller';
 import { ProfileModule } from './profile/profile.module';
+import { JenispengeluaranModule } from './jenispengeluaran/jenispengeluaran.module';
+import { JenisPengeluaranModule } from './jenis-pengeluaran/jenis-pengeluaran.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, PrismaModule, ProfileModule],
+  imports: [AuthModule, UsersModule, PrismaModule, ProfileModule, JenispengeluaranModule, JenisPengeluaranModule],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
