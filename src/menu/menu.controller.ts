faaa4371b34311292976a013f3af454050633a3e
Heru Anggara <heruanggara0909@gmail.com>
2024-05-29 20:31:11 +0700

    feat: menu, add controller

---
+++ src/menu/menu.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { MenuService } from './menu.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+
+@Controller('menu')
+export class MenuController {
+    constructor (private menuService: MenuService) {}
+    
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    async getLevelsWithMenus() {
+        return this.menuService.index();
+    }
+    
+    @UseGuards(JwtAuthGuard)
+    @Post(':levelId/change-permission')
+    async changePermission(@Param('levelId') levelId: number, @Body() menuId: string) {
+        return this.menuService.changePermission(levelId, menuId);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Patch('update-position')
+    async updatePositionMenu(@Body('position') position: number, @Body() menuId: string) {
+        return this.menuService.updatePositionMenu(position, menuId);
+    }
+}
