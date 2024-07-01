ea29567e55702f43b58bffff5c2f4ec02a828b86
Heru Anggara <heruanggara0909@gmail.com>
2024-07-01 16:46:12 +0700

    feat: add merchandise dto

---
+++ src/merchandise/dto/merchandise.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class MerchandiseDto {
+    @IsNotEmpty()
+    @IsString()
+    nama: string
+
+    @IsNotEmpty()
+    @IsNumber()
+    nilai: number
+
+    @IsNotEmpty()
+    @IsString()
+    gambar: string
+}
